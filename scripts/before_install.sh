5da6e7d7315ea421865c4996a8c41eaf47ff1c67
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2021-05-23 20:12:20 -0700

    Add scripts for CI CD instance to EC2

---
+++ scripts/before_install.sh
@@ -0,0 +1,12 @@
+FILE=/home/ubuntu/app/startup.json
+if test -f "$FILE"; then
+   echo "$FILE exists"
+   cd /home/ubuntu/
+   pm2 delete 0
+   killall -9 node
+else 
+    echo "$FILE does not exist."
+fi
+cd /home/ubuntu/
+sudo rm -rf app
+sudo mkdir app
